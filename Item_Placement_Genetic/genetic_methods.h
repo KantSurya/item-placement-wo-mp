f93e60d94ac9f8ab21ea07a52151ef81a87dc665
surya <suryaskant29@gmail.com>
2023-04-07 14:02:08 +0530

    Add crossover, utility methods, fix bugs

--- Item_Placement_Genetic/genetic_methods.h
+++ Item_Placement_Genetic/genetic_methods.h
@@ -2,6 +2,7 @@
     MUTATION
 */
 
+// Implemented for Distinct item in each cell. 
 vector<vector<int>> mutation_rsm(vector<vector<int>>warehouse){
     int n = warehouse.size();
     if(n<1){
@@ -9,12 +10,9 @@ vector<vector<int>> mutation_rsm(vector<vector<int>>warehouse){
     }
     int m = warehouse[0].size();
 
-    Cell i = Cell();
-    Cell j = Cell();
+    Cell i = GetRandomCell(n,m);
+    Cell j = GetRandomCell(n,m);
 
-    i.Setter(rand(0,n-1) , rand(0,m-1));
-    j.Setter(rand(0,n-1) , rand(0,m-1));
- 
     if (j < i) {
         swap(i,j);
     }
@@ -35,7 +33,69 @@ vector<vi> mutation_psm(vector<vi>warehouse){
     return {};
 }
 
+// Implemented for Distinct item in each cell. 
 vector<vi> crossover(vector<vi> a, vector<vi> b){
-    // TODO : implement me 
-    return {};
+    int n = a.size();
+    if(n<1){
+        return {{-1}};
+    }
+    int m = a[0].size();
+
+    vector<vi>child(n,vi(m));
+    Cell i = Cell(0,0);
+
+    map< int, Cell >itemIndexA, itemIndexB;
+    set<int>itemSet;
+
+    fi(0,n-1){
+        fj(0,m-1){
+            itemSet.insert(b[i][j]);
+
+            itemIndexB[ b[i][j] ] = Cell(i,j);
+            itemIndexA[ a[i][j] ] = Cell(i,j);
+        }
+    }
+    
+    Cell startInd = GetRandomCell(n,m);
+    Cell invalidInd = Cell(-1,-1);
+
+    int currItem = a[startInd.x][startInd.y];
+    child[i.x][i.y] = currItem;
+
+    i = GetNextCell(i,n,m);
+    itemSet.erase(currItem);
+
+    while(itemSet.size()>0){
+        Cell nextCellA = GetNextCell(itemIndexA[currItem],n,m);
+        Cell nextCellB = GetNextCell(itemIndexB[currItem],n,m);
+
+        itemIndexA[currItem] = invalidInd;
+        itemIndexB[currItem] = invalidInd;
+
+        int nextItemA = a[nextCellA.x][nextCellA.y];
+        int nextItemB = b[nextCellB.x][nextCellB.y];
+
+        // keep incrementing the index untill cell with item is found
+        while( IsEqualCell(itemIndexA[nextItemA] , invalidInd)){
+            nextCellA = GetNextCell(nextCellA,n,m);
+            nextItemA = a[nextCellA.x][nextCellA.y];
+        }
+    
+        while( IsEqualCell(itemIndexB[nextItemB] , invalidInd)){
+            nextCellB = GetNextCell(nextCellB,n,m);
+            nextItemB = b[nextCellB.x][nextCellB.y];
+        }
+
+        int nextItem = nextItemA;
+        if(distance(i,nextCellB) < distance(i,nextCellA)){
+            nextItem = nextItemB;
+        }
+
+        child[i.x][i.y] = nextItem;
+        i = GetNextCell(i,n,m);
+
+        currItem = nextItem;
+        itemSet.erase(currItem);
+    }
+    return child;  
 }
--- Item_Placement_Genetic/utility.h
+++ Item_Placement_Genetic/utility.h
@@ -2,10 +2,14 @@
 // ------------------------------------------------------------------------------------------------ 
 #define debug(x) cout<<#x<<": "<<x<<endl;
 #define DEBUG(x) cout<<#x<<": \n"; for(int i = 0 ; i < x.size() ; ++i)cout<<x[i]<<" \n"[i==(int)x.size()-1];
+#define debugCell(c) cout<<#c<<": "<<c.x<<" "<<c.y<<endl;
+
 #define fi(a,b) for(int i = a ; i<=b ; ++i)
 #define fj(a,b) for(int j = a ; j<=b ; ++j)
+
 #define vi vector<int>
 #define vvi vector<vi>
+#define pii pair<int,int>
 // ------------------------------------------------------------------------------------------------ 
 
 
@@ -23,10 +27,9 @@ template<class T>
     }
 // ------------------------------------------------------------------------------------------------ 
 
-// ------------------------------------------------------------------------------------------------ 
-#define debugCell(cell) cout<<cell.x<<" "<<cell.y<<endl;
-
-// ------------------------------------------------------------------------------------------------ 
+bool IsEqualCell(Cell a, Cell b){
+    return (a.x==b.x && a.y==b.y);
+}
 
 Cell GetNextCell(Cell cell,int n,int m){
     cell.y++;
@@ -34,6 +37,10 @@ Cell GetNextCell(Cell cell,int n,int m){
     if( cell.y == m){
         cell.x++;
         cell.y = 0;
+    }
+
+    if(cell.x==n){
+        cell.x = 0;
     }
 
     return cell;
@@ -47,9 +54,43 @@ Cell GetPrevCell(Cell cell,int n,int m){
         cell.y = m-1;
     }
 
+    if(cell.x < 0){
+        cell.x = n-1;
+    }
+    
     return cell;
+}
+
+Cell GetRandomCell(int n,int m){
+    Cell i = Cell( rand(0,n-1) , rand(0,m-1) );
+
+    return i;
+}
+
+pii GetRandomPair(int n,int m){
+    pair<int,int>p = {rand(0,n-1) , rand(0,m-1)};
+
+    return p;
 }
 
 int distance(Cell& a, Cell& b){
     return abs(a.x - b.x) + abs(a.y - b.y);
+}
+
+vector<vi>GetRandomMember(int n,int m){
+    vector<int>ar(n*m);
+    for(int i = 0 ; i < n*m ; ++i){
+        ar[i] = i;
+    }
+    shuffle(ar.begin(),ar.end(),rng);
+
+    vector<vi>randomMember(n,vi(m));
+    for(int i = 0 ; i < n*m ; ++i){
+        int x = i/m;
+        int y = i%m;
+
+        randomMember[x][y] = ar[i];
+    }
+
+    return randomMember;
 }
